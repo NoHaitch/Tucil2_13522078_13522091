d849842e3c9d6dd91146afd1b5c47ed69138af92
Chiko but no H <franciscotrianto@gmail.com>
2024-03-17 23:52:57 +0700

    feat: gui validation, divide and conquer animation

--- src/style.py
+++ src/style.py
@@ -1,25 +1,29 @@
 # Tkinter Style
-window_width = 500
-window_height = 500
 
 # Text style
 font_family = "Courier New"
 font_size_title = 24
+font_size_subtitle = 18
 font_size_normal = 14
 
 # Chart Style
-line_width = 2
-color_control_point = "#C34468"
+bezier_line_width = 2
+control_point_line_width = 1
+bezier_circle_radius = 40
+control_point_circle_radius = 25
+color_control_point = "#231929"
 color_bezier_point = "#10B3E6"
 empty_space_chart = 5
 
 # For Exporting variables
-__all__ = ['window_width',
-           'window_height',
-           'font_size_title',
+__all__ = ['font_size_title',
+            'font_size_subtitle',
            'font_size_normal',
            'font_family', 
-           'line_width',
+           'bezier_line_width',
+           'control_point_line_width',
+           'bezier_circle_radius',
+           'control_point_circle_radius',
            'color_control_point',
            'color_bezier_point',
            'empty_space_chart']
--- src/utils.py
+++ src/utils.py
@@ -1,32 +1,94 @@
 # Global Import Definition
 import tkinter as tk
+from tkinter import Tk, messagebox, Entry
+
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # Class Definition
 class Point:
-    def __init__(self, x:float, y:float):
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
     def __str__(self):
         return f"({self.x},{self.y})"
-    
+
     def __eq__(self, otherPoint) -> bool:
         return (self.x == otherPoint.x) and (self.y == otherPoint.y)
-    
-def midPoint(p1: Point, p2: Point) -> Point:
-    midP = Point(((p1.x+p2.x) /2),((p1.y+p2.y)/2))
+
+# Global Function
+def is_float(string: str) -> bool:
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+# Get the mid point between two point
+def mid_point(p1: Point, p2: Point) -> Point:
+    midP = Point(((p1.x + p2.x) / 2), ((p1.y + p2.y) / 2))
     return midP
 
-def printList(lis : list):
-    for i in lis:
-        print(i)
+# Partition for Quicksort
+def partition(points : list[Point], begin: int, end: int) -> int:
+    pivot = begin
+    for i in range(begin + 1, end + 1):
+        if points[i].x <= points[begin].x:
+            pivot += 1
+            points[i], points[pivot] = points[pivot], points[i]
+    points[pivot], points[begin] = points[begin], points[pivot]
+    return pivot
 
-#used to calculate how many points in bfBezier
-def pointCalculator(n: int) -> int:
-    if(n == 1):
+# Quicksort for points
+def quicksort(points : list[Point], begin : int = 0, end : int = None) -> None:
+    if end is None:
+        end = len(points) - 1
+
+    def _quicksort(points : list[Point], begin: int, end: int) -> None:
+        if begin >= end:
+            return
+        pivot = partition(points, begin, end)
+        _quicksort(points, begin, pivot - 1)
+        _quicksort(points, pivot + 1, end)
+
+    return _quicksort(points, begin, end)
+
+# used to calculate how many points in bfBezier
+def calculate_amount_of_point(n: int) -> int:
+    if n == 1:
         return 3
     else:
-        return (pointCalculator(n-1)*2)-1
-    
+        return (calculate_amount_of_point(n - 1) * 2) - 1
+
+# Binary search for points
+def binary_search(points : list[Point], value : Point) -> int:
+    low = 0
+    high = len(points) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if points[mid].x < value.x:
+            low = mid + 1
+        elif points[mid].x > value.x:
+            high = mid - 1
+        else:
+            if points[mid].y < value.y:
+                low = mid + 1
+            elif points[mid].y > value.y:
+                high = mid - 1
+            else:
+                return mid
+    return low
+
+# Split an array at value
+def split_array(points : list[Point], value : Point) -> tuple[list[Point], list[Point]]:
+    index = binary_search(points, value)
+    left = points[:index]
+    right = points[index:]
+    return left, right
+
+# Debugging Tool
+def print_list(lis: list[Point]) -> None:
+    for i in lis:
+        print(i)

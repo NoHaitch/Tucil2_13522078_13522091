91d38efa2456b8cc88a4fe7c4661e9d4a8b2df1c
Venantius Sean Ardi Nugroho <seanardi4@gmail.com>
2024-03-18 21:33:55 +0700

    feat: bug fix

--- src/dcBezier.py
+++ src/dcBezier.py
@@ -15,22 +15,20 @@ def intermediary_Bezier(control_points: list[Point], desire_iteration: int, num_
 
 def dcBuilder(control_points: list[Point], container: list, counter: int, desire_iteration: int, num_of_CP: int) -> None:
     if counter < desire_iteration:
-        useful_midpoints = [control_points[0], control_points[-1]]
-        midpoint = make_mid_point(control_points, num_of_CP, useful_midpoints)
-
-        quicksort(useful_midpoints)
-        leftPoints, rightPoints = split_array(useful_midpoints, midpoint)
-        leftPoints.append(midpoint)
+        leftPoints= [control_points[0]]
+        rightPoints = [control_points[-1]]
+        midpoint = make_mid_point(control_points, num_of_CP, leftPoints,rightPoints)
         counter += 1
 
         dcBuilder(leftPoints, container, counter, desire_iteration, num_of_CP)
         container.append(midpoint)
         dcBuilder(rightPoints, container, counter, desire_iteration, num_of_CP)
 
-def make_mid_point(control_points: list[Point], num_of_CP: int, useful_midpoints: list[Point]) -> Point:
+def make_mid_point(control_points: list[Point], num_of_CP: int, useful_midpoints_a: list[Point],useful_midpoints_b: list[Point]) -> Point:
     if num_of_CP == 2:
         real_midpoint = mid_point(control_points[0], control_points[1])
-        useful_midpoints.append(real_midpoint)
+        useful_midpoints_a.append(real_midpoint)
+        useful_midpoints_b.insert(0,real_midpoint)
         return real_midpoint
     
     else:
@@ -38,7 +36,8 @@ def make_mid_point(control_points: list[Point], num_of_CP: int, useful_midpoints
         for i in range(num_of_CP - 1):
             points_between.append(mid_point(control_points[i], control_points[i + 1]))
 
-        useful_midpoints.append(points_between[0])
-        useful_midpoints.append(points_between[-1])
+        useful_midpoints_a.append(points_between[0])
+        useful_midpoints_b.insert(0,points_between[-1])
         num_of_CP -= 1
-        return make_mid_point(points_between, num_of_CP, useful_midpoints)
+        return make_mid_point(points_between, num_of_CP, useful_midpoints_a,useful_midpoints_b)
+
--- src/utils.py
+++ src/utils.py
@@ -31,63 +31,12 @@ def mid_point(p1: Point, p2: Point) -> Point:
     midP = Point(((p1.x + p2.x) / 2), ((p1.y + p2.y) / 2))
     return midP
 
-# Partition for Quicksort
-def partition(points : list[Point], begin: int, end: int) -> int:
-    pivot = begin
-    for i in range(begin + 1, end + 1):
-        if points[i].x <= points[begin].x:
-            pivot += 1
-            points[i], points[pivot] = points[pivot], points[i]
-    points[pivot], points[begin] = points[begin], points[pivot]
-    return pivot
-
-# Quicksort for points
-def quicksort(points : list[Point], begin : int = 0, end : int = None) -> None:
-    if end is None:
-        end = len(points) - 1
-
-    def _quicksort(points : list[Point], begin: int, end: int) -> None:
-        if begin >= end:
-            return
-        pivot = partition(points, begin, end)
-        _quicksort(points, begin, pivot - 1)
-        _quicksort(points, pivot + 1, end)
-
-    return _quicksort(points, begin, end)
-
 # used to calculate how many points in bfBezier
 def calculate_amount_of_point(n: int) -> int:
     if n == 1:
         return 3
     else:
         return (calculate_amount_of_point(n - 1) * 2) - 1
-
-# Binary search for points
-def binary_search(points : list[Point], value : Point) -> int:
-    low = 0
-    high = len(points) - 1
-
-    while low <= high:
-        mid = (low + high) // 2
-        if points[mid].x < value.x:
-            low = mid + 1
-        elif points[mid].x > value.x:
-            high = mid - 1
-        else:
-            if points[mid].y < value.y:
-                low = mid + 1
-            elif points[mid].y > value.y:
-                high = mid - 1
-            else:
-                return mid
-    return low
-
-# Split an array at value
-def split_array(points : list[Point], value : Point) -> tuple[list[Point], list[Point]]:
-    index = binary_search(points, value)
-    left = points[:index]
-    right = points[index:]
-    return left, right
 
 # Debugging Tool
 def print_list(lis: list[Point]) -> None:
